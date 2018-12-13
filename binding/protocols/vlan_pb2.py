249aa7f48465212ebcb00464eeafaced11cbe92a
kruiz2015 <42749339+kruiz2015@users.noreply.github.com>
2018-12-12 23:24:57 -0500

    Update vlan_pb2.py

--- binding/protocols/vlan_pb2.py
+++ binding/protocols/vlan_pb2.py
@@ -2,7 +2,7 @@
 #kruiz2015@my.fit.edu
 #Vlan_class
 class Vlan:
-	def __init__(self, is_override_tpid = False, tpid = 1, vlan_tag = 0):
+	def __init__(self, is_override_tpid = False, tpid = 0, vlan_tag = 0):
 		self.is_override_tpid = bool(is_override_tpid)
 		self.tpid = int(tpid)
 		self.vlan_tag = int(vlan_tag)	
