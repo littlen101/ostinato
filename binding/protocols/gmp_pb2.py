4f9a0bd9a715eda54bd81eb198a37732d7ebd3a4
Awwinstead <awwinstead@gmail.com>
2018-12-13 11:41:59 -0500

    Update gmp_pb2.py

--- binding/protocols/gmp_pb2.py
+++ binding/protocols/gmp_pb2.py
@@ -6,52 +6,6 @@
 #Corrisponding API guide:
 #https://apiguide.ostinato.org/module_gmp.html
 
-class Gmp:
-    #Class Constants
-    #GroupMode
-    kFixed = 0
-    kIncrementGroup = 1
-    kDecrementGroup = 2
-    kRandomGroup = 3
-
-    #RecordType
-    kReserved = 0
-    kIsInclude = 1
-    kIsExclude = 2
-    kToInclude = 3
-    kToExclude = 4
-    kAllowNew = 5
-    kBlockOld = 6
-
-    #Had to use type1 cause type is restricted pyhton name.
-    def __init__(self, type1 = 0, is_override_rsvd_code = False,
-                 rsvd_code = 0, max_response_time = 100,
-                 is_override_checksum = False, checksum = 0,
-                 group_address, group_mode = kFixed, group_count = 16,
-                 group_prefix = 24, s_flag = False, qrv = 2, qqi = 125,
-                 is_override_source_count = False, source_count = 0,
-                 group_records, is_override_group_record_count = False,
-                 group_records = 0):
-        self.type1 = int(type1)
-        self.is_override_rsvd_code = bool(is_override_rsvd_code)
-        self.rsvd_code = int(rsvd_code)
-        self.max_response_time = int(max_response_time)
-        self.is_override_checksum = bool(is_override_checksum)
-        self.checksum = int(checksum)
-        self.group_address = IpAddress(group_address)
-        self.group_mode = GroupMode(group_mode)
-        self.group_count = int(group_count)
-        self.group_prefix = int(group_prefix)
-        self.s_flag = bool(s_flag)
-        self.qrv = int(qrv)
-        self.qqi = int(qqi)
-        self.sources = List[IpAddress](sources)
-        self.is_override_source_count = bool(is_override_source_count)
-        self.source_count = int(source_count)
-        self.group_records = List[GroupRecord](group_records)
-        self.is_override_group_record_count = bool(is_override_group_record_count)
-        self.group_record_count = int(group_record_count)
-        return
 
 class GroupRecord:
     class type:
@@ -76,6 +30,55 @@ class IpAddress:
         self.v4 = int(v4)
         self.v6_lo = int(v6_lo)
         self.v6_hi = int(v6_hi)
+        return
+
+
+
+class Gmp:
+    #Class Constants
+    #GroupMode
+    kFixed = 0
+    kIncrementGroup = 1
+    kDecrementGroup = 2
+    kRandomGroup = 3
+
+    #RecordType
+    kReserved = 0
+    kIsInclude = 1
+    kIsExclude = 2
+    kToInclude = 3
+    kToExclude = 4
+    kAllowNew = 5
+    kBlockOld = 6
+
+    #Had to use type1 cause type is restricted pyhton name.
+    def __init__(self, type1 = 0, is_override_rsvd_code = False,
+                 rsvd_code = 0, max_response_time = 100,
+                 is_override_checksum = False, checksum = 0,
+                 group_address = IpAddress[], group_mode = kFixed, group_count = 16,
+                 group_prefix = 24, s_flag = False, qrv = 2, qqi = 125,
+                 is_override_source_count = False, source_count = 0,
+                 sources = IpAddress[], group_records = [], is_override_group_record_count = False,
+                 group_record_count = 0):
+        self.type1 = int(type1)
+        self.is_override_rsvd_code = bool(is_override_rsvd_code)
+        self.rsvd_code = int(rsvd_code)
+        self.max_response_time = int(max_response_time)
+        self.is_override_checksum = bool(is_override_checksum)
+        self.checksum = int(checksum)
+        self.group_address = IpAddress(group_address)
+        self.group_mode = GroupMode(group_mode)
+        self.group_count = int(group_count)
+        self.group_prefix = int(group_prefix)
+        self.s_flag = bool(s_flag)
+        self.qrv = int(qrv)
+        self.qqi = int(qqi)
+        self.sources = List[IpAddress](sources)
+        self.is_override_source_count = bool(is_override_source_count)
+        self.source_count = int(source_count)
+        self.group_records = List[GroupRecord](group_records)
+        self.is_override_group_record_count = bool(is_override_group_record_count)
+        self.group_record_count = int(group_record_count)
         return
 
 
